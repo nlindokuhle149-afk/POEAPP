0b1a138a23c1a1bbf4ef0b0b9ee2161bedae8e6c
Student <Student@LabVM2049939.tsng3churt5u1h0ip0dwgd0fma.jnbx.internal.cloudapp.net>
2026-09-18 20:42:31 +0200

    initial commit

---
+++ src/main/java/com/mycompany/poeapp/Login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poeapp;
+
+/**
+ *
+ * @author Student
+ */
+public class Login {
+     private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String cellPhoneNumber;
+    
+}
