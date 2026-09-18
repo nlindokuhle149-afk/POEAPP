a14d5ee106269b0aa3ae4b7e9946ad4d6a3d4ce1
Student <Student@LabVM2049939.tsng3churt5u1h0ip0dwgd0fma.jnbx.internal.cloudapp.net>
2026-09-18 23:00:04 +0200

    initial commit

--- src/main/java/com/mycompany/poeapp/Login.java
+++ src/main/java/com/mycompany/poeapp/Login.java
@@ -14,5 +14,64 @@ public class Login {
     private String userName;
     private String password;
     private String cellPhoneNumber;
+   
+    //consructor 
+    public Login(){
+    }
     
-}
+    //constuctor used to store registration details 
+    public Login(String firstName,String lastName,String userName,String password,
+            String cellPhoneNumber){
+        
+    this.firstName = firstName;
+    this.lastName = lastName;
+     this.userName = userName;
+     this.password = password;
+    this.cellPhoneNumber = cellPhoneNumber;        
+    }
+    //----------------------------------------------------------------------
+    // checkuserName()
+    //-----------------------------------------------------------------------
+    public boolean checkuserName(){
+        /*
+        * user must   
+        *
+        */
+        
+        return userName != null
+                && userName.contains("_")
+                && userName.length()<=5;
+    }
+    //----------------------------------------------------------------------
+    //check password
+    //-----------------------------------------------------------------------
+    public boolean checkPasswordComplexity(){
+        /* PASSSWORD must:
+        * 1.be at least 8 characters long
+        * 2.contain a capital letter
+        * 3.contain a number
+        * 4. contain a special character
+        */
+        if (password == null || password.length()<8){
+            
+        }
+        boolean hascapitalLetter = false;
+         boolean hasNumber = false;
+        boolean   hasspecialcharacter = false;
+        
+        for(int i = 0;i <password.length();i++){
+            char Character = password.charAt(i);
+            
+       if (Character.isUpperCase(character)){
+           hascapitalLetter= true;
+       }if (Character.isDigit(character)){
+           hasNumber = true;
+       }if (!Character.isLetterorDigit(character)){
+           hasspecialcharacter = true;
+       }
+        }
+    
+       
+                
+            
+                    
